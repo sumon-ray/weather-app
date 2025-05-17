00a345e18c31dd6c583a7194c9aa646c8e79eeb3
sumon-ray <raysumon220020@gmail.com>
2025-05-17 09:29:26 +0600

    added temparature details based on current location

--- app/[location]/@aqi/page.js
+++ app/[location]/@aqi/page.js
@@ -1,5 +1,10 @@
-const AQIPage = () => {
-    return <div>AQIPage</div>;
+import AQIComponent from "@/app/components/AQIComponent";
+
+const AQIPage = ({
+  params: { location },
+  searchParams: { latitude, longitude },
+}) => {
+  return <AQIComponent lat={latitude} lon={longitude} />;
 };
 
 export default AQIPage;
--- app/[location]/@temperature/page.js
+++ app/[location]/@temperature/page.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import TemperatureComponent from "@/app/components/TemperatureComponent";
 
-const TemperaturePage = () => {
-    return (
-        <div>
-            temperature
-        </div>
-    );
+const TemperaturePage = ({
+  params: { location },
+  searchParams: { latitude, longitude },
+}) => {
+  return <TemperatureComponent lat={latitude} lon={longitude} />;
 };
 
 export default TemperaturePage;
